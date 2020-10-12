f5e6cea271de363834a98abcbaf944092a05dadd
edmund27 <epafko@gmail.com>
2020-10-12 16:55:52 +0200

    Connected to server with socket.io

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const ctx = gameCanvas.getContext("2d");
 
 const playButton = document.getElementById('play-button');
 
+const socket = io("http://localhost:3000")
+
+socket.on("connected", (message) => {
+    console.log("From server:", message)
+})
+
 ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
 ctx.strokestyle = CANVAS_BORDER_COLOUR;
 
